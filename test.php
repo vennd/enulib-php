ec271c4690bbbe7eeba31e242d0ef6916ace57a9
Jeremy Lam <jeremy@vennd.io>
2015-04-18 01:40:37 +1000

    Added address generation call

--- test.php
+++ test.php
@@ -11,7 +11,8 @@ $enulib = new enuLib();
 // Create a unique payment identifier that can be used to query the status of the payment later
 $paymentId = bin2hex(openssl_random_pseudo_bytes(10, $cstrong));
 
-// Create payment
+// Create payment which sends to the given address, the given amount of the asset. The payment id is a unique identifier which you can use later to look up the payment
+// The last parameter is the miner's fee (if applicable) in satoshis
 $result = $enulib->createPayment('1DYucjPoEVzu7FMnwFVpKcvP9wo9ZYeF7u', 1200000000, 'NEKO', $paymentId, 1500);
 
 if ($result{'code'} == 0) {
@@ -26,5 +27,9 @@ else {
     printf("Failed to create payment\n");
     print($result);
 }
+
+// Create a new source address from which payments can be made. After the new source address is generated, the default address from which payments will be made is this address
+//$newSourceAddress = $enulib->createAddress();
+//printf("New source address generated: %s \n", $newSourceAddress['value'])
 
 ?>
